e2766fcd5ee0410d5761acfc2fc3504e34e0baad
Barsbekteam <barsbekbarsbek99@gmail.com>
2022-10-22 13:26:50 +0300

    create

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function() {
+    const Accordion = function(el, multiple) {
+        this.el = el || {};
+        this.multiple = multiple || false;
+
+        // Variables privadas
+        const links = this.el.find('.link');
+        // Evento
+        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+    }
+
+    Accordion.prototype.dropdown = function(e) {
+        const $el = e.data.el;
+        $this = $(this),
+            $next = $this.next();
+
+        $next.slideToggle();
+        $this.parent().toggleClass('open');
+
+        if (!e.data.multiple) {
+            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+        };
+    }
+
+    const accordion = new Accordion($('#accordion'), false);
+});
