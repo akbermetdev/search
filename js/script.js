99b943ea6dc9b0b503d2d602dbeae4f81fa23293
Barsbekteam <barsbekbarsbek99@gmail.com>
2022-10-22 20:21:03 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -1,26 +1,8 @@
-$(function() {
-    const Accordion = function(el, multiple) {
-        this.el = el || {};
-        this.multiple = multiple || false;
-
-        // Variables privadas
-        const links = this.el.find('.link');
-        // Evento
-        links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
-    }
-
-    Accordion.prototype.dropdown = function(e) {
-        const $el = e.data.el;
-        $this = $(this),
-            $next = $this.next();
-
-        $next.slideToggle();
-        $this.parent().toggleClass('open');
-
-        if (!e.data.multiple) {
-            $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-        };
-    }
-
-    const accordion = new Accordion($('#accordion'), false);
-});
+const btn = document.querySelector('.collection__btn--button')
+btn.addEventListener('click', () => {
+    document.querySelector('.collection__modal').style.display = 'block'
+})
+const close = document.querySelector('.collection__modal--bg__square--close')
+close.addEventListener('click', () => {
+    document.querySelector('.collection__modal').style.display = 'none'
+})
